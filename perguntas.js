c8521b861613def669193ac13a921dda0cd72a37
kochx1 <koch.arthur@escola.pr.gov.br>
2025-09-09 08:46:22 -0300

    09-09

---
+++ perguntas.js
@@ -0,0 +1,66 @@
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Conhecimentos gerias', 
+    'Quem pintou a obra "Mona Lisa"?',
+    'Leonardo da Vinci'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
+criaCartao(
+    'categoria', 
+    'pergunta',
+    'resposta'
+) 
+
